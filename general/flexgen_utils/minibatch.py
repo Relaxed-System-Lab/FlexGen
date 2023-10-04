f41aa4a8a95199c2a01106588884eba410eee94f
dingfangyu <1224795746@qq.com>
2023-10-04 08:55:26 +0000

    minibatch

---
+++ general/flexgen_utils/minibatch.py
@@ -0,0 +1,44 @@
+import torch 
+
+def get_size_info(obj): # recursive
+    if isinstance(obj, tuple):
+        return tuple(get_size_info(o) for o in obj)
+    elif isinstance(obj, list):
+        return list(get_size_info(o) for o in obj)
+    elif isinstance(obj, dict):
+        return {k:get_size_info(v) for k, v in obj.items()}
+    elif isinstance(obj, torch.Tensor):
+        return obj.size()
+    else:
+        return type(obj)
+
+def get_kth_batch_inputs(inputs, k, gpu_batch_size): # for both args, kwargs
+    if isinstance(inputs, tuple): # e.g. args
+        return tuple(get_kth_batch_inputs(inp, k, gpu_batch_size) for inp in inputs)
+    elif isinstance(inputs, dict): # e.g. kwargs
+        return {key:get_kth_batch_inputs(value, k, gpu_batch_size) for key, value in inputs.items()}
+    elif isinstance(inputs, torch.Tensor):
+        return inputs[k * gpu_batch_size:(k + 1) * gpu_batch_size]
+    elif isinstance(inputs, int) or inputs is None: 
+        return inputs
+    else:
+        raise NotImplementedError(f'inputs: {inputs} of type \'{type(inputs)}\' is not implemented.')
+
+def concat_outputs(outputs: list): # concat K outputs to one output
+    if isinstance(outputs[0], torch.Tensor):
+        return torch.cat(outputs, dim=0)
+    elif isinstance(outputs[0], tuple):
+        ans = []
+        for elem in zip(*outputs):
+            if isinstance(elem[0], torch.Tensor):
+                ans.append(torch.cat(elem, dim=0))
+            elif isinstance(elem[0], tuple):
+                ans.append(concat_outputs(elem))
+            elif isinstance(elem[0], int): # all the same
+                ans.append(elem[0])
+            else:
+                NotImplementedError(f'outputs concat function of type {type(elem[0])} is not implemented.')
+        return tuple(ans) 
+    else:
+        raise NotImplementedError(f'outputs concat function of type {type(outputs[0])} is not implemented.')
+
