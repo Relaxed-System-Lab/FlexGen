07fb7d89fb3645f33789297d524f8b2271995413
dingfangyu <1224795746@qq.com>
2023-10-31 08:53:14 +0000

    test cuda

---
+++ general/run-gpu.py
@@ -0,0 +1,28 @@
+from utils import Policy, logging
+from wrapper import FlexGen
+from utils.test import test_hf_gen
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+checkpoint = "facebook/opt-125m"  # 125m 6.7b 13b 30b
+# checkpoint = "Salesforce/codegen-350M-mono"
+# checkpoint = 'bigscience/bloom-560m' #
+
+policy = Policy(
+    gpu_batch_size=1,
+    num_gpu_batches=4,
+    weights_gpu_percent=0.2,
+    weights_cpu_percent=0.3,
+    cache_gpu_percent=0.3,
+    cache_cpu_percent=0.2,
+    act_gpu_percent=0.4,
+    act_cpu_percent=0.5,
+    overlap=True,
+    pin_weight=True,
+)
+compute_device = "cuda:0"
+
+with FlexGen(checkpoint, policy, compute_device=compute_device) as model:
+    num_prompts = policy.gpu_batch_size * policy.num_gpu_batches
+    test_hf_gen(checkpoint, model, num_prompts, compute_device)
