372b96c0cb00f3d47a903b0b4b80e0c071b101ac
dingfangyu <1224795746@qq.com>
2024-04-28 13:39:04 +0800

    plots of throughputs and strategies

--- flexgen/profile_matmul.py
+++ flexgen/profile_matmul.py
@@ -10,23 +10,24 @@ from flexgen.profile_bandwidth import benchmark_func
 
 
 def bench_matmul():
-    for device in ["cuda", "cpu"]:
-        for n in [1024, 2048]:
-            if device == "cuda":
-                dtype = torch.float16
-            else:
-                dtype = torch.float32
+    for device in ["cuda"]:
+        for dtype in [torch.float16, torch.float32]:
+            for n in [1024, 2048, 7184]:
+                # if device == "cuda":
+                #     dtype = torch.float16
+                # else:
+                #     dtype = torch.float32
 
-            a = torch.rand(n, n).to(dtype).to(device)
-            b = torch.rand(n, n).to(dtype).to(device)
+                a = torch.rand(n, n).to(dtype).to(device)
+                b = torch.rand(n, 64 * 2024).to(dtype).to(device)
 
-            def func():
-                return torch.matmul(a, b)
+                def func():
+                    return torch.matmul(a, b)
 
-            cost = np.mean(benchmark_func(func, number=5, repeat=3))
+                cost = np.mean(benchmark_func(func, number=1, repeat=1, warmup=3)) 
 
-            tflops = 2 * n * n * n / cost / 1e12
-            print(f"device: {device}, N: {n}, latency: {cost*1e3:.2f} ms, TFLOPS: {tflops:.3f}")
+                tflops = 2 * a.numel() * b.shape[-1] / cost / 1e12
+                print(f"dtype: {dtype}, device: {device}, N: {n}, latency: {cost*1e3:.2f} ms, TFLOPS: {tflops:.3f}")
         print()
 
 
