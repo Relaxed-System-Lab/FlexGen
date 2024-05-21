f7bcb401f8a1d898fc66d150cb484271ec5fdbfe
dingfangyu <1224795746@qq.com>
2024-05-21 18:46:13 +0800

    profile bdw

--- flexgen/profile_bandwidth.py
+++ flexgen/profile_bandwidth.py
@@ -2,25 +2,22 @@
 Usage:
 bash /usr/local/bin/pagecache-management.sh python3 profile_bandwidth.py
 """
-# sudo sh -c 'echo 3 >  /proc/sys/vm/drop_caches'  
 
 import argparse
 import numpy as np
 import os
 import time
 import torch
+from safetensors.torch import load_file, save_file
 
 from flexgen.utils import GB, MB, KB
 
-from safetensors.torch import save_file, load_file
 
-from torch.cuda.nvtx import range_push, range_pop
-
-def benchmark_func(func, number, repeat, warmup=0):
+def benchmark_func(func, number, repeat, warmup=3):
     for i in range(warmup):
         func()
 
-    costs = []
+    costs = [0]
 
     for i in range(repeat):
         torch.cuda.synchronize()
@@ -34,57 +31,60 @@ def benchmark_func(func, number, repeat, warmup=0):
 
 
 def profile_bandwidth(path):
-    s, h = 2048, 7184
+    s, h = 512, 512
     path_dir = os.path.dirname(path)
     os.makedirs(path_dir, exist_ok=True)
 
-    links = [("cpu", "gpu"), ("gpu", "cpu"), #("gpu", "gpu"), ("cpu", "cpu"),
-             ("cpu", "disk"),("cpu", "safetensors"), ("disk", "cpu")]
+    links = [("cpu", "gpu"), ("gpu", "cpu"), ("gpu", "gpu"), ("cpu", "cpu"),
+             ("cpu", "np.memmap"), ("np.memmap", "cpu"), ("cpu", "safetensors"), ("safetensors", "cpu")]
 
     for (dst, src) in links:
-        for b in [1, 32, 256, ]:
+        for b in [1, 128, 512]:
             if dst == "cpu":
                 dst_tensor = torch.ones((b, s, h), dtype=torch.int8, pin_memory=True)
             elif dst == "gpu":
                 dst_tensor = torch.ones((b, s, h), dtype=torch.int8, device="cuda:0")
-            elif dst == "disk":
-                np.lib.format.open_memmap(path + '_d', mode="w+", shape=((b,s,h)), dtype=np.int8)
-                dst_tensor = path + '_d'
-            # elif dst == 'safetensors':...
+            elif dst == "np.memmap":
+                np.lib.format.open_memmap(path, mode="w+", shape=((b,s,h)), dtype=np.int8)
+                dst_tensor = path
+            elif dst == "safetensors":
+                dst_tensor = path
 
             if src == "cpu":
                 src_tensor = torch.ones((b, s, h), dtype=torch.int8, pin_memory=True)
             elif src == "gpu":
                 src_tensor = torch.ones((b, s, h), dtype=torch.int8, device="cuda:0")
-            elif src == "disk":
-                if not os.path.exists(path + f'_d_{b}_{s}_{h}'):
-                    np.lib.format.open_memmap(path +f'_d_{b}_{s}_{h}', mode="w+", shape=((b,s,h)), dtype=np.int8)
-                src_tensor = path + f'_d_{b}_{s}_{h}'
+            elif src == "np.memmap":
+                np.lib.format.open_memmap(path, mode="w+", shape=((b,s,h)), dtype=np.int8)
+                src_tensor = path
             elif src == 'safetensors':
-                if not os.path.exists(path + f'_s_{b}_{s}_{h}'):
-                    save_file({'data': torch.ones((b, s, h), dtype=torch.int8, pin_memory=True)}, path + f'_s_{b}_{s}_{h}')
-                src_tensor = path + f'_s_{b}_{s}_{h}'
+                data = torch.ones((b, s, h), dtype=torch.int8, pin_memory=True)
+                save_file({'data': data}, path)
+                src_tensor = path
 
             dst_indices = (slice(0, b), slice(0, s), slice(0, h))
             src_indices = (slice(0, b), slice(0, s), slice(0, h))
 
             def func():
-                if isinstance(src_tensor, str) and src == 'disk':
-                    src_tensor_ = torch.from_numpy(np.lib.format.open_memmap(src_tensor))
-                elif isinstance(src_tensor, str) and src == 'safetensors':
-                    src_tensor_ = load_file(src_tensor)['data']
+                if isinstance(src_tensor, str):
+                    if src == 'np.memmap':
+                        src_tensor_ = torch.from_numpy(np.lib.format.open_memmap(src_tensor))
+                    elif src == 'safetensors':
+                        src_tensor_ = load_file(src_tensor)['data']
                 else:
                     src_tensor_ = src_tensor
                 if isinstance(dst_tensor, str):
-                    dst_tensor_ = torch.from_numpy(np.lib.format.open_memmap(dst_tensor))
+                    if dst == 'np.memmap':
+                        dst_tensor_ = torch.from_numpy(np.lib.format.open_memmap(dst_tensor))
+                    elif dst == 'safetensors':
+                        save_file({'data': src_tensor_}, path)
+                        return 
                 else:
                     dst_tensor_ = dst_tensor
                 dst_tensor_[dst_indices].copy_(src_tensor_[src_indices])
 
             size = np.prod([(x.stop - x.start) / (x.step or 1) for x in dst_indices])
-            range_push(f'{dst, src = }')
-            cost = np.mean(benchmark_func(func, number=1, repeat=1, warmup=0))
-            range_pop()
+            cost = np.mean(benchmark_func(func, number=5, repeat=3))
             bandwidth = size / cost / GB
 
             print(f"size: {size / MB:6.2f} MB, {src}-to-{dst} bandwidth: {bandwidth:.3f} GB/s")
@@ -93,7 +93,7 @@ def profile_bandwidth(path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--offload-path", type=str, default="./flexgen_offload_dir/tmp")
+    parser.add_argument("--offload-path", type=str, default="~/flexgen_offload_dir/tmp.npy")
     args = parser.parse_args()
 
     profile_bandwidth(os.path.expanduser(args.offload_path))
