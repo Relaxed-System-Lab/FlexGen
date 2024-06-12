f8257fd4afc558009e2da0cc21b2ed334ce54b50
dingfangyu <1224795746@qq.com>
2024-06-12 15:08:06 +0800

    disk vector _max_len

--- test/vector.py
+++ test/vector.py
@@ -1,12 +1,42 @@
-import torch 
+import torch
+import numpy as np
+from numpy.lib.format import open_memmap
+
+# Dict of NumPy dtype -> torch dtype (when the correspondence exists)
+numpy_to_torch_dtype_dict = {
+    # np.bool       : torch.bool,
+    np.uint8      : torch.uint8,
+    np.int8       : torch.int8,
+    np.int16      : torch.int16,
+    np.int32      : torch.int32,
+    np.int64      : torch.int64,
+    np.float16    : torch.float16,
+    np.float32    : torch.float32,
+    np.float64    : torch.float64,
+    np.complex64  : torch.complex64,
+    np.complex128 : torch.complex128
+}
+
+# Dict of torch dtype -> NumPy dtype
+torch_to_numpy_dtype_dict = {value : key for (key, value) in numpy_to_torch_dtype_dict.items()}
+
 
 class Vector:
+    _max_len = None
+
+    @classmethod
+    def set_disk_vector_max_len(cls, max_len: int):
+        # if we use disk vector, we want a fixed vector length 
+        # to avoid copying between memmap file storages
+        cls._max_len = max_len
+
     def __init__(self, 
         data_shape: list[int], 
-        dtype: torch.dtype, 
+        dtype: torch.dtype | np.dtype, 
         device: torch.device | str | int, 
         dim: int, 
-        cap: int | None = None 
+        cap: int | None = None,
+        **kwargs
     ):
         self.data_shape = data_shape # mutable
         self.dtype = dtype
@@ -21,29 +51,59 @@ class Vector:
             raise ValueError('dim error')
 
         # capacity of storage
-        if cap is None:
+        if device == 'disk' and cap is None:
+            assert self._max_len is not None, \
+                'try to call Vector.set_disk_vector_max_len(max_len) in advance.'
+            self.cap = self._max_len
+        elif cap is not None:
+            self.cap = cap
+        else: 
+            # default: 1.5 x data_length
             self.cap = data_shape[dim] * 3 // 2
-        else:
-            self.cap = cap  
         
         # init storage
         self.storage_shape = [s if d != self.dim else self.cap for d, s in enumerate(self.data_shape)] # mutable
-        self.pin_memory = self.device in ['cpu', torch.device('cpu')]
-        self.storage = torch.zeros(self.storage_shape, dtype=self.dtype, device=self.device, pin_memory=self.pin_memory)
+
+        if self.device != 'disk':
+            # cpu | gpu
+            self.pin_memory = self.device in ['cpu', torch.device('cpu')]
+
+            self.storage = torch.zeros(self.storage_shape, dtype=self.dtype, device=self.device, pin_memory=self.pin_memory)
+        else:
+            # disk
+            self.file_name = kwargs.get("file_name")
+            self.mmap = open_memmap(self.file_name, shape=tuple(self.storage_shape), dtype=self.dtype, mode='w+')
+
+            self.storage = torch.from_numpy(self.mmap)
         
     @property 
     def rear(self):
+        return self.data_shape[self.dim]  
+    
+    def length(self):
         return self.data_shape[self.dim]  
     
     def empty(self):
         return self.rear == 0
     
     @classmethod
-    def from_tensor(cls, tensor: torch.Tensor, dim: int):
-        vec = cls(list(tensor.shape), tensor.dtype, tensor.device, dim)
+    def from_tensor(cls, tensor: torch.Tensor, dim: int, device=None, **kwargs):
+        device = device if device is not None else tensor.device # default to tensor.dtype
+        dtype = tensor.dtype if device != 'disk' else torch_to_numpy_dtype_dict[tensor.dtype] # torch.dtype | np.dtype
+        
+        vec = cls(data_shape=list(tensor.shape), dtype=dtype, device=device, dim=dim, **kwargs)
         indices = [slice(0, s) for s in tensor.shape]
         vec.storage[*indices].copy_(tensor[*indices]) 
+        
+        if device == 'disk':
+            vec.mmap.flush()
+
         return vec
+
+    def move_to_device(self, device, **kwargs):
+        tmp = self.from_tensor(self.data, self.dim, device, **kwargs)
+        self.__dict__ = tmp.__dict__
+        return self
 
     @property
     def shape(self):
@@ -92,6 +152,8 @@ class Vector:
 
         push_len = x.shape[self.dim]
         if self.rear + push_len > self.cap:
+            if self.device == 'disk':
+                raise RuntimeError("disk vector oom")
             self.increase_storage(push_len)
             
         push_slice = [slice(None) for _ in range(len(self.storage_shape))]
@@ -115,32 +177,10 @@ class Vector:
         self.data_shape[self.dim] -= pop_len  
 
         if self.rear < self.cap // 2:
-            self.shrink_storage() 
+            if self.device != 'disk':
+                self.shrink_storage() 
 
         return ret
-
-    def can_do_pop_and_push(self, vec_to_push):
-        if self.dim != vec_to_push.dim:
-            return False
-        return self.check_copyable(vec_to_push.storage)
-
-    def pop_and_push(self, pop_len: int, vec_to_push):
-        assert self.can_do_pop_and_push(vec_to_push)
-        pop_slice = [slice(None) for _ in range(len(self.storage_shape))]
-        assert self.rear - pop_len >= 0
-        pop_slice[self.dim] = slice(self.rear - pop_len, self.rear)
-        pop_data = self.storage[pop_slice]
-
-        vec_to_push.push_back(pop_data) # pop and push
-
-        self.data_shape[self.dim] -= pop_len  
-        if self.rear < self.cap // 2:
-            self.shrink_storage() 
-
-    def push_to_other_vec(self, vec_to_push):
-        assert self.can_do_pop_and_push(vec_to_push)
-        push_data = self.data
-        vec_to_push.push_back(push_data) 
 
     def __repr__(self) -> str:
         return f"{self.data.tolist()}"
