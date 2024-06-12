31dba552c9cbb6bdbef8dde05d034dda90bf2dd0
dingfangyu <1224795746@qq.com>
2024-06-12 14:59:24 +0800

    disk vector _max_len

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ watch -n 0.1 nvidia-smi
 
 find . -size +1M | sed 's|^\./||g' | cat > .gitignore 
 
-find . -size +100k | sed 's|^\./||g' | cat > .gitignore && git add . && git commit -m 'disk vector' && git push
+find . -size +100k | sed 's|^\./||g' | cat > .gitignore && git add . && git commit -m 'disk vector _max_len' && git push
